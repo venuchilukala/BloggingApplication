f27a7c6ebab20014b3f9607a613cf468d0a38f2d
venuchilukala <venuchilukala07@gmail.com>
2025-02-13 17:24:46 +0530

    nav added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = 8000
 
+app.set("view engine", "ejs")
+app.set("views", path.resolve('./views'))
+
+app.get('/', (req, res) => {
+    return res.render("home")
+})
 
 app.listen(PORT, () => {
     console.log(`Server Started at : http://localhost:${PORT}`)
